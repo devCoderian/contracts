20d8526a2f652464e45c8c42737e7d3c9394ea87
devCoderian <devcoderian@gmail.com>
2022-02-05 23:40:47 +0900

    initial setting

---
+++ MintAnimalToken.sol
@@ -0,0 +1,7 @@
+// SPDX-Licence-Identifier : MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzepplin/contracts/token/ERC721/extensions/ERC721Enumerable.sol"
+
+contract MintAnimalToken is ERC721Enumerable
