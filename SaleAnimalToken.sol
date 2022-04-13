68690fd84c77e06a45b713095ef810dbfba0d87d
LAPTOP-U0CO45L6\user <devcoderian@gmail.com>
2022-04-13 15:26:22 +0900

    구매함수 작성 완료

--- SaleAnimalToken.sol
+++ SaleAnimalToken.sol
@@ -37,26 +37,36 @@ contract SaleAnimalToken{
 
         //구매 함수
         function purchaseAnimalToken(uint256 _animalTokenId) public payable {
-            uint256 price = animalTokenPrices[_animalTokenId];
-            address animalTokenOwner = mintAnimalTokenAddress.ownerOf(_animalTokenId);
+            //payable => 코인들이 거래되는 함수를 사용할 수 있음
+            uint256 price = animalTokenPrices[_animalTokenId]; //가격을 꺼내옴
+            address animalTokenOwner = mintAnimalTokenAddress.ownerOf(_animalTokenId); //주인의 주소값 불러오기
             
-            require(price > 0, "Animal token not sale.");
-            require(price <= msg.value, "Caller sent lower than price");
-            require(animalTokenOwner != msg.sender, "Caller is animal token owner.");
+            require(price > 0, "Animal token not sale."); //price가 0보다 작다는 것은 token이 판매 등록 되어있지 않다는 뜻
+            require(price <= msg.value, "Caller sent lower than price"); //price가 msg.value보다 작거나 같을 때 통과 , msg.value =>metice의 양
+            require(animalTokenOwner != msg.sender, "Caller is animal token owner."); //구매할 때는 오너가 msg.sender이 아니어야 구현할 수 있음. 
 
+            //구매 로직이 전송됨.
             payable(animalTokenOwner).transfer(msg.value);
 
+            //소유자 변경
             mintAnimalTokenAddress.safeTransferFrom(animalTokenOwner, msg.sender, _animalTokenId);
 
+            //매핑에서 제거하는 법 = 0처리
             animalTokenPrices[_animalTokenId] = 0;
             
+             //배열의 길이만큼 반복
             for(uint256 i = 0; i<onSaleAnimalTokenArray.length; i++){
+                // 방금 0원으로 초기화했기 때문에 유일하게 0인 아이를 찾아서 제거해준다.
+                // 배열 인덱스에서 맨뒤의 값과 교체한 후 pop()으로 제거해준다.
                 if(animalTokenPrices[onSaleAnimalTokenArray[i]]== 0){
                     onSaleAnimalTokenArray[i] = onSaleAnimalTokenArray[onSaleAnimalTokenArray.length - 1];
                     onSaleAnimalTokenArray.pop();
                 }
             }
         }
+
+        //프론트엔드에서 사용할 판매중인 토큰의 길이를 출력하는 함수 view처리
+        //프론트에서 이 함수를 이용해 판매중인리스트를 가져오기위해 onSaleAnimalTokenArray를 for문을 돌린다.
 
         function getOnSaleAnimalTokenArrayLength() view public returns(uint256){
             return onSaleAnimalTokenArray.length;
