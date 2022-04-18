5828ce75ea08c3322913800598a24b93587682ee
LAPTOP-U0CO45L6\user <devcoderian@gmail.com>
2022-04-18 17:29:05 +0900

    컨트랙트 리팩토링 수정

--- MintAnimalToken.sol
+++ MintAnimalToken.sol
@@ -1,30 +1,78 @@
-// SPDX-Licence-Identifier : MIT
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract MintAnimalToken is ERC721Enumerable{
-    constructor() ERC721("IANAnimals", "IAN"){}
+import "SaleAnimalToken.sol";
 
-    mapping(uint256 => string) private animal;
+contract MintAnimalToken is ERC721Enumerable {
+    constructor() ERC721("MINT", "IAN") {}
+
+    SaleAnimalToken public saleAnimalToken;
+
     mapping(uint256 => uint256) public animalTypes;
     // mapping이란?
     // 매핑은 일반적인 프로그래밍 언어에서는 해시테이블이나 사전과 유사합니다.
     // 키(Key) - 값(value) 형태로 쌍으로 저장되고 제공된 키(Key)를 가지고 값(value)을 얻어낼 수 있습니다.
     // tokenId를 입력하면 animalType이 반환된다.
 
-    function mintAnimalToken() public { 
-        uint256 animalTokenId = totalSupply() + 1;
+    //외부 함수 사용을 위해
+    struct AnimalTokenData {
+        uint256 animalTokenId;
+        uint256 animalType;
+        uint256 animalPrice;
+    }
 
-        uint256 animalType = uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender, animalTokenId))) % 5 + 1; 
+    function mintAnimalToken() public {
+        //총 발행량의 + 1 => 유일한 값
+        uint256 animalTokenId = totalSupply() + 1; 
+
+        uint256 animalType = uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender, animalTokenId))) % 5 + 1;
         //keccak256을 사용하기 위해서 byte코드가 필요하다.
-        //값 3개를 합쳐서 해쉬화하한 랜덤값이 나온다.
+        //값 3개를 합쳐서 해쉬화된 랜덤값이 나온다.
         //%5 하면 0-4까지의 값이 나오고 + 1 => 부터 5의 랜덤값 뽑기
 
         //랜덤값을 매핑에 집어넣기
         animalTypes[animalTokenId] = animalType;
-        _mint(msg.sender, animalTokenId);
-    }   
 
+        //ERC721에서 제공해주는 minting 함수
+        //이 명령어를 실행한 사람 => 민팅 버튼 누른 사람
+        _mint(msg.sender, animalTokenId);
+    }
+
+    //리팩토링 함수 => 재 deploy
+    //리턴 타입 memory 명시(storage => 영구 저장, memory => 임시)
+    function getAnimalTokens(address _animalTokenOwner) view public returns (AnimalTokenData[] memory) {
+        
+        //소유갯수 체크
+        uint256 balanceLength = balanceOf(_animalTokenOwner);
+        
+        require(balanceLength != 0, "Owner did not have token.");
+
+        //balanceLength 배열 생성
+        AnimalTokenData[] memory animalTokenData = new AnimalTokenData[](balanceLength);
+
+        //
+        for(uint256 i = 0; i < balanceLength; i++) {
+            // 토큰 아이디, types, price 가져오기
+            uint256 animalTokenId = tokenOfOwnerByIndex(_animalTokenOwner, i);
+            uint256 animalType = animalTypes[animalTokenId];
+            //saleAnimal Token은 mintAnimalToken deploy 후 생성하기 때문에 생성자 단계에서 생성할 수 없다.
+            //import 후 SaleAnimalToken saleAnimalToken 생성 후 함수(setSaleAnimalToken)를 하나 더 만들어주어야한다. 
+            //getAnimalTokenPrice함수(mintAnimalToken내에서 struct 생성)를 saleAnimalToken.sol 내에서 생성해주어야 한다.
+
+            uint256 animalPrice = saleAnimalToken.getAnimalTokenPrice(animalTokenId);
+
+            animalTokenData[i] = AnimalTokenData(animalTokenId, animalType, animalPrice);
+        }
+
+        return animalTokenData;
+    }
+    
+    // import 후 SaleAnimalToken saleAnimalToken 생성 후 함수를 하나 더 만들어주어야한다. 
+    // 사용은 remix에서 setSaleAnimalToken 따로 설정 
+    function setSaleAnimalToken(address _saleAnimalToken) public {
+        saleAnimalToken = SaleAnimalToken(_saleAnimalToken);
+    }
 }
--- SaleAnimalToken.sol
+++ SaleAnimalToken.sol
@@ -4,70 +4,74 @@ pragma solidity ^0.8.0;
 
 import "MintAnimalToken.sol";
 
-contract SaleAnimalToken{
-    // MintAnimalToken을 deploy하게 되면 deploy한 주소값을 담는다.
+contract SaleAnimalToken {
+
+    //deploy한 주소를 담는다.
     MintAnimalToken public mintAnimalTokenAddress;
-    constructor (address _mintAnimalTokenAddress){
+
+    constructor (address _mintAnimalTokenAddress) {
         mintAnimalTokenAddress = MintAnimalToken(_mintAnimalTokenAddress);
     }
 
-    mapping(uint256 => uint256) public animalTokenPrices; //animalTokenId를 입력하면 실제 가격을 리턴한다.
 
-    //frontend에서 어떤게 판매중인 토큰인지 확인할 수 있도록 만든다.
+    mapping(uint256 => uint256) public animalTokenPrices; //tokenId를 입력하면 가격을 리턴한다.
+
+    //프론트엔드에서 어떤게 판매중인 토큰인지 알 수 있도록 배열 생성   
     uint256[] public onSaleAnimalTokenArray;
 
-    function setForSaleAnimalToken(uint256 _animalTokenId, uint256 _price) public{ //다 참조할 수 있게끔 public 처리
-        address animalTokenOwner = mintAnimalTokenAddress.ownerOf(_animalTokenId); //토큰 아이디 값으로 주인이 누군지 알려주는 함수
-            
+    function setForSaleAnimalToken(uint256 _animalTokenId, uint256 _price) public { //다 참조할 수 있게끔 퍼블릭 처리
+        address animalTokenOwner = mintAnimalTokenAddress.ownerOf(_animalTokenId); //토큰 아이디 값으로 주인 주소 식별
+
+        //true 일떄 , 뒤에 실행하고 funcrion setForSaleAnimalToken 실행 종료
+        //require안에 있는 내용이 거짓이면 실행 중단
         require(animalTokenOwner == msg.sender, "Caller is not animal token owner."); //토큰 주인이 판매요청 한 사람과 동일한지 알려주는 함수
-        require(_price > 0, "Price is zero or lower."); // 가격이 0보다 크게 넣을 지 확인
-        //3번째 조건 만들기 전 가격들을 관리하는 매핑
-        require(animalTokenPrices[_animalTokenId] == 0, "This animal token is already on sale.");  //값이 0일 경우 유효하지 않다. 이미 팔린 토큰인지 확인
+        require(_price > 0, "Price is zero or lower."); //가격이 0보다 크게 넣을 지 확인
+        require(animalTokenPrices[_animalTokenId] == 0, "This animal token is already on sale."); //값이 0일 경우 유효하지 않다. 이미 팔린 토큰인지 확인
         require(mintAnimalTokenAddress.isApprovedForAll(animalTokenOwner, address(this)), "Animal token owner did not approve token."); //위임 권한 확인
-        //isApprovedForAll(token주인, SaleAnimalToken Contract의 주소) 이 주인이 판매 계약서에 판매 권한을 넘겼는지, 다른 스마트 컨트랙트로 넘어가게 하지 않기 위해
-        //boolean값으로 반환됨 true일때만 통과돼서 판매 등록이 될 수 있다. 
+        //(token주인, SaleAnimalToken Contract의 주소) 주인이 판매계약서에 판매 권한을 넘겼는지, 다른 스마트 컨트랙트로 넘어가게 하지 않기 위해
+        //boolean값으로 반환됨.
+        //setApprovedforAll = true처리
 
-        
+        //_animalTokenId에 해당하는 값을 넣어주기
         animalTokenPrices[_animalTokenId] = _price;
-        
-        //front단 사용
-        onSaleAnimalTokenArray.push(_animalTokenId); //판매중인 토큰은 animalTokenId를 집어넣는다.
 
+        //프론트엔드에서 보여주기 위해
+        onSaleAnimalTokenArray.push(_animalTokenId);
     }
 
-        //구매 함수
-        function purchaseAnimalToken(uint256 _animalTokenId) public payable {
-            //payable => 코인들이 거래되는 함수를 사용할 수 있음
-            uint256 price = animalTokenPrices[_animalTokenId]; //가격을 꺼내옴
-            address animalTokenOwner = mintAnimalTokenAddress.ownerOf(_animalTokenId); //주인의 주소값 불러오기
-            
-            require(price > 0, "Animal token not sale."); //price가 0보다 작다는 것은 token이 판매 등록 되어있지 않다는 뜻
-            require(price <= msg.value, "Caller sent lower than price"); //price가 msg.value보다 작거나 같을 때 통과 , msg.value =>metice의 양
-            require(animalTokenOwner != msg.sender, "Caller is animal token owner."); //구매할 때는 오너가 msg.sender이 아니어야 구현할 수 있음. 
+    
+    //구매 처리
+    function purchaseAnimalToken(uint256 _animalTokenId) public payable {
+        
+        // 토큰 아이디값 => 가격 조회
+        uint256 price = animalTokenPrices[_animalTokenId];
 
-            //구매 로직이 전송됨.
-            payable(animalTokenOwner).transfer(msg.value);
+        // token owner address = tokenId owner 뽑아오기
+        address animalTokenOnwer = mintAnimalTokenAddress.ownerOf(_animalTokenId);
 
-            //소유자 변경
-            mintAnimalTokenAddress.safeTransferFrom(animalTokenOwner, msg.sender, _animalTokenId);
+        
+        require(price > 0, "Animal token not sale."); //
+        require(price <= msg.value, "Caller sent lower than price."); //
+        require(animalTokenOnwer != msg.sender, "Caller is animal token owner.");
 
-            //매핑에서 제거하는 법 = 0처리
-            animalTokenPrices[_animalTokenId] = 0;
-            
-             //배열의 길이만큼 반복
-            for(uint256 i = 0; i<onSaleAnimalTokenArray.length; i++){
-                // 방금 0원으로 초기화했기 때문에 유일하게 0인 아이를 찾아서 제거해준다.
-                // 배열 인덱스에서 맨뒤의 값과 교체한 후 pop()으로 제거해준다.
-                if(animalTokenPrices[onSaleAnimalTokenArray[i]]== 0){
-                    onSaleAnimalTokenArray[i] = onSaleAnimalTokenArray[onSaleAnimalTokenArray.length - 1];
-                    onSaleAnimalTokenArray.pop();
-                }
+        payable(animalTokenOnwer).transfer(msg.value);
+        mintAnimalTokenAddress.safeTransferFrom(animalTokenOnwer, msg.sender, _animalTokenId);
+
+        animalTokenPrices[_animalTokenId] = 0;
+
+        for(uint256 i = 0; i < onSaleAnimalTokenArray.length; i++) {
+            if(animalTokenPrices[onSaleAnimalTokenArray[i]] == 0) {
+                onSaleAnimalTokenArray[i] = onSaleAnimalTokenArray[onSaleAnimalTokenArray.length - 1];
+                onSaleAnimalTokenArray.pop();
             }
         }
+    }
 
-        //프론트엔드에서 사용할 판매중인 토큰의 길이를 출력하는 함수 view처리
-        //프론트에서 이 함수를 이용해 판매중인리스트를 가져오기위해 onSaleAnimalTokenArray를 for문을 돌린다.
-        function getOnSaleAnimalTokenArrayLength() view public returns(uint256){
-            return onSaleAnimalTokenArray.length;
-        }
+    function getOnSaleAnimalTokenArrayLength() view public returns (uint256) {
+        return onSaleAnimalTokenArray.length;
+    }
+
+    function getAnimalTokenPrice(uint256 _animalTokenId) view public returns (uint256) {
+        return animalTokenPrices[_animalTokenId];
+    }
 }
